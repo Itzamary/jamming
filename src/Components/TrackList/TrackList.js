4ff30353673fc7f77f53903f2d39ca1d096f27e6
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2023-03-23 23:00:42 -0500

    created a Tracklist component and its accompanying css file.

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './TrackList.css';
+import { Track } from '../Track';
+
+
+export class TrackList extends React.Component {
+    render () {
+        return (
+            <div className='TrackList'>
+                <ul>
+                    <li>Gatubela</li>
+                    <li>lo siento bebe</li>
+                    <li>ella perrea sola</li>
+                </ul>
+            </div>
+        )
+    }
+}
