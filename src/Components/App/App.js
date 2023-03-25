de36d3d716dec5437c8d7614dd7a5872c1af9fa0
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2023-03-24 20:38:21 -0500

    added state to app component and passed it down as a prop to other components till it reached the track component in which the info was rendered in.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,30 @@ import { SearchResults } from '../SearchResults/SearchResults';
 import { Playlist } from '../Playlist/Playlist';
 
 export class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = { 
+      searchResults: [
+      {
+        name: 'yo Perrro sola',
+        artist: 'Bad Bunny',
+        album: 'PereeoLand',
+        id: 1
+      },
+      {
+        name: 'song1',
+        artist: "artist1",
+        album: 'album1',
+        id: 2
+      },
+      {
+        name: 'song2',
+        artist: "artist2" ,
+        album: 'album2',
+        id: 3
+      }
+    ]}
+  }
   render() {
     return (
       <div>
@@ -12,7 +36,7 @@ export class App extends React.Component{
         <div className="App">
           < SearchBar />
           <div className="App-playlist">
-            < SearchResults />
+            < SearchResults searchResults={this.state.searchResults}/>
             < Playlist />
           </div>
         </div>
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './TrackList.css';
-//import { Track } from '../Track/Track';
+import { Track } from '../Track/Track';
 
 
 export class TrackList extends React.Component {
+
     render () {
         return (
             <div className='TrackList'>
-                <ul>
-                    <li>Gatubela</li>
-                    <li>lo siento bebe</li>
-                    <li>ella perrea sola</li>
-                </ul>
+                {
+                    this.props.tracks?.map((track) => {
+                        console.log(this.props.tracks);
+                        console.log(track, 'track');
+                         return <Track track={track} key={track.id} />
+                    })
+                }
             </div>
         )
     }
