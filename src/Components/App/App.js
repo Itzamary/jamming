f84bef92b04e9daead844414bb0f8982543c4c33
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2023-03-26 21:22:14 -0500

    created a method for saving the playlist using the uri of the playlistTracks tracks. passed the method to the playlist component as a prop to be use in the onClick attribute.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -15,6 +15,7 @@ export class App extends React.Component{
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -37,6 +38,10 @@ export class App extends React.Component{
     this.setState({ playlistName: name });
   }
 
+  savePlaylist() {
+    const trackURIs = this.state.playlistTracks.map(track => track.uri);
+  }
+
   render() {
     return (
       <div>
@@ -45,7 +50,7 @@ export class App extends React.Component{
           < SearchBar />
           <div className="App-playlist">
             < SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            < Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} />
+            < Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
